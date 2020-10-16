74f45b3d5a7bd42c668aa430a295f0d17829eaee
ivandana <70627290+ivandana@users.noreply.github.com>
2020-10-16 09:38:37 -0700

    corrected the functions.

--- task2.py
+++ task2.py
@@ -1,10 +1,15 @@
 import random
 
-def multiplication_game():
+# Function to generate tuple of two random numbers.
+def random_tuple_gen():
+    #generates a single digit number from 0 to 9
+    tup_num = (random.randrange(1, 10), random.randrange(1, 10))
+    return tup_num
+
+#Function to validate user input unless he enters right value.
+def multiplication_game(num1, num2):
     while True:
-        (num1, num2) = random.randrange(0, 10), random.randrange(0, 10)
         output = int(input(f'How much is {num1} times {num2}? '))
-    
         result = num1 * num2
         if result == output:
             print('Done.')
@@ -12,4 +17,6 @@ def multiplication_game():
         else:
             print(f'{num1} times {num2} is not {output}, please try again.' )  
 
-multiplication_game()
+#Calling Function random_tuple_gen and storing the tuple in variable. 
+tuple_num = random_tuple_gen()
+multiplication_game(tuple_num[0], tuple_num[1])
